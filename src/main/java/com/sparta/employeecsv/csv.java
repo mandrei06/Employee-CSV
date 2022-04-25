59b9bcb9cadb01bc393aab6e73641d851a6f5bf4
mandrei06 <marincasandrei06@gmail.com>
2022-04-25 17:24:16 +0100

    Extract data from CSV and create a list of Employee objects

--- src/main/java/com/sparta/employeecsv/csv.java
+++
@@ -1,4 +0,0 @@
-package com.sparta.employeecsv;
-
-public class csv {
-}
---
+++ src/main/java/com/sparta/employeecsv/model/Employee.java
@@ -0,0 +1,102 @@
+package com.sparta.employeecsv.model;
+
+import java.sql.Date;
+
+public class Employee {
+    public int id;
+    public String prefix;
+    public String firstName;
+    public char middleInitial;
+    public String lastName;
+    public char gender;
+    public String email;
+    public Date dateOfBirth;
+    public Date dateOfJoining;
+    public float salary;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getPrefix() {
+        return prefix;
+    }
+
+    public void setPrefix(String prefix) {
+        this.prefix = prefix;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public char getMiddleInitial() {
+        return middleInitial;
+    }
+
+    public void setMiddleInitial(char middleInitial) {
+        this.middleInitial = middleInitial;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public char isGender() {
+        return gender;
+    }
+
+    public void setGender(char gender) {
+        this.gender = gender;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Date getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(Date dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public Date getDateOfJoining() {
+        return dateOfJoining;
+    }
+
+    public void setDateOfJoining(Date dateOfJoining) {
+        this.dateOfJoining = dateOfJoining;
+    }
+
+    public float getSalary() {
+        return salary;
+    }
+
+    public void setSalary(float salary) {
+        this.salary = salary;
+    }
+
+
+
+
+
+
+}
