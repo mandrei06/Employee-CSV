62e1c5bca558fcd52c8730452cef3f5b41a9a6a5
mandrei06 <marincasandrei06@gmail.com>
2022-04-26 18:32:20 +0100

    Create an different HashMap to keep the track of duplicates

--- src/main/java/com/sparta/employeecsv/controller/Main.java
+++ src/main/java/com/sparta/employeecsv/controller/Main.java
@@ -5,14 +5,13 @@ import com.sparta.employeecsv.model.Employee;
 import com.sparta.employeecsv.view.EmployeeDisplay;
 
 import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
         HashMap<Integer, Employee> employees = CSVExtract.readCSV("EmployeeRecords.csv");
         // Employee List
         EmployeeDisplay.employeesDisplay(employees, employees);
+
         // Employee Duplicate List
         EmployeeDisplay.employeesDisplay(CSVExtract.duplicates, employees);
     }
